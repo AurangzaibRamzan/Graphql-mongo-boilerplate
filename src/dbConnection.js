d87c999faa6cb7eacaa2cf8f13efb62255b0b121
Aurangzaib <Aurangzaib987@gmail.com>
2020-03-24 18:59:47 +0500

    minor changes

--- src/dbConnection.js
+++ src/dbConnection.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export default function connectDatabase() {
   try {
-    mongoose.connect('mongodb://localhost:27017/Graphql-mongo',
+    mongoose.connect('mongodb+srv://aurangzaib:03346284563@mongodb01-3ppnp.mongodb.net/test?retryWrites=true&w=majority',
       { useNewUrlParser: true });
   } catch (error) {
     console.log('MongoDB Connection Error', error);
